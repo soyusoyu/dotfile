025abe4ea113231ceae0c8c2dbf6b2945657727c
soga yuichi <soga@soganoMacBook-Pro.local>
2023-10-05 13:18:26 +0900

    neovim_setting.sh更新

--- neovim_setting.sh
+++ neovim_setting.sh
@@ -1,9 +1,9 @@
 cp ~/.bash_profile ~/.bash_profile.neovim_setting.sh.bak
+cp ~/.zshrc ~/..zshrc.neovim_setting.sh.bak
 # echo 'export XDG_CONFIG_HOME=$HOME/.config' >> ~/.bash_profile
-source ~/.bash_profile
+# source ~/.bash_profile
 mkdir -p ${HOME}/.config
-# ln -snfv ${HOME}/.vim ${HOME}/.config/nvim
-# ln -snfv ${HOME}/.vimrc ${HOME}/.config/nvim/init.vim
-ln -snfv ${HOME}/dotfile/.vim ${HOME}/.config/nvim
-ln -snfv ${HOME}/dotfile/.vimrc ${HOME}/.config/nvim/init.vim
 
+ln -snfv ${HOME}/dotfile/config/nvim ${HOME}/.config/nvim
+ln -snfv ${HOME}/dotfile/starship.toml ${HOME}/.config/starship.toml
+ln -snfv ${HOME}/dotfile/.zshrc ${HOME}/.zshrc
