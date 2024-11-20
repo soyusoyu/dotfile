900f7b1f8d7d5758f50854fe5b7f60ff7e7ad181
soyusoyu <soyusoyu@example.com>
2024-11-20 10:09:20 +0900

    my-setting.lua変更

--- config/nvim/lua/custom/plugins/my-setting.lua
+++ config/nvim/lua/custom/plugins/my-setting.lua
@@ -6,6 +6,10 @@ vim.keymap.set('n', '<C-h>', '<C-w>h', { noremap = true })
 vim.keymap.set('n', '<C-j>', '<C-w>j', { noremap = true })
 vim.keymap.set('n', '<C-k>', '<C-w>k', { noremap = true })
 vim.keymap.set('n', '<C-l>', '<C-w>l', { noremap = true })
+
+-- gFで新規タグで開く
+vim.api.nvim_set_keymap('n', 'gF', '<cmd>tabnew <cfile><cr>', { noremap = true })
+
 -- 検索結果のハイライトを解除する
 vim.keymap.set('n', '<Esc><Esc>', ':nohlsearch<CR>', { silent = true })
 
@@ -15,6 +19,12 @@ vim.api.nvim_set_keymap("n", ":um",
   {noremap = true, silent = true})
 vim.api.nvim_set_keymap("n", ":ut",
   [[<cmd>lua require('telescope-tabs').list_tabs()<CR>]],
+  {noremap = true, silent = true})
+vim.api.nvim_set_keymap("n", ":rg",
+  [[<cmd>tabnew <CR>:grep]],
+  {noremap = true, silent = true})
+vim.api.nvim_set_keymap("n", ":tm",
+  [[<cmd>tabnew <CR>:terminal<CR>]],
   {noremap = true, silent = true})
 
 -- swap関連ファイルは作成しない
@@ -42,6 +52,8 @@ _G.my_tabline = function()
   return s
 end
 
+vim.o.tags = './tags'
+
 -- Set tabline and showtabline options
 vim.o.tabline = '%!v:lua.my_tabline()'
 vim.o.showtabline = 2
@@ -50,5 +62,13 @@ if vim.fn.executable('rg') == 1 then
   vim.o.grepprg = 'rg --vimgrep --no-heading'
   vim.o.grepformat = '%f:%l:%c:%m,%f:%l:%m'
 end
+
+function CopyFilePathToClipboard()
+    local file_path = vim.fn.expand('%:p')  -- 完全なパスを取得
+    vim.fn.setreg('+', file_path)  -- クリップボードにコピー
+    print('Copied to clipboard: ' .. file_path)
+end
+vim.api.nvim_set_keymap('n', 'cpf', ':lua CopyFilePathToClipboard()<CR>', { noremap = true, silent = true })
+
 
 return {}
